6fd8fd99f9cd3d4be1360d712bacb5f793052d50
axeladelwyn <85653279+axeladelwyn@users.noreply.github.com>
2024-08-10 11:47:18 +0700

    added javascript logic

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,27 @@
-console.log("Hello from the external JavaScript file!");
+function calculateBMI() {  
+    const weight = parseFloat(document.getElementById('weight').value);  
+    const heightInCm = parseFloat(document.getElementById('height').value);  
+    const resultElement = document.getElementById('result');  
+
+    if (weight > 0 && heightInCm > 0) {  
+        const heightInM = heightInCm / 100; // Convert height from cm to m  
+        const bmi = weight / (heightInM * heightInM);  
+        let category = '';  
+
+        if (bmi < 18.5) {  
+            category = 'Severely Underweight';  
+        } else if (bmi < 24.9) {  
+            category = 'Normal';  
+        } else if (bmi < 29.9) {  
+            category = 'Overweight';  
+        } else {  
+            category = 'Obese';  
+        }  
+
+        resultElement.innerHTML = `Your BMI is ${bmi.toFixed(2)} - ${category}`;  
+    } else {  
+        resultElement.innerHTML = 'Please enter valid weight and height.';  
+    }  
+}  
+
+
