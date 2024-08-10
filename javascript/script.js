9b633ffc5cd8adaceba2dbe0511bb4cd71354b41
axeladelwyn <85653279+axeladelwyn@users.noreply.github.com>
2024-08-10 13:20:15 +0700

    center text

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,31 @@
-function calculateBMI() {  
-    const weight = parseFloat(document.getElementById('weight').value);  
-    const heightInCm = parseFloat(document.getElementById('height').value);  
-    const resultElement = document.getElementById('result');  
+function calculateBMI() {
+  const weight = parseFloat(document.getElementById("weight").value);
+  const heightInCm = parseFloat(document.getElementById("height").value);
+  const resultElement = document.getElementById("result");
 
-    if (weight > 0 && heightInCm > 0) {  
-        const heightInM = heightInCm / 100; // Convert height from cm to m  
-        const bmi = weight / (heightInM * heightInM);  
-        let category = '';  
+  if (weight > 0 && heightInCm > 0) {
+    const heightInM = heightInCm / 100; // Convert height from cm to m
+    const bmi = weight / (heightInM * heightInM);
+    let category = "";
 
-        if (bmi < 18.5) {  
-            category = 'Severely Underweight';  
-        } else if (bmi < 24.9) {  
-            category = 'Normal';  
-        } else if (bmi < 29.9) {  
-            category = 'Overweight';  
-        } else {  
-            category = 'Obese';  
-        }  
+    if (bmi < 18.5) {
+      category = "Severely Underweight";
+    } else if (bmi < 24.9) {
+      category = "Normal";
+    } else if (bmi < 29.9) {
+      category = "Overweight";
+    } else {
+      category = "Obese";
+    }
 
-        resultElement.innerHTML = `Your BMI is ${bmi.toFixed(2)} - ${category}`;  
-    } else {  
-        resultElement.innerHTML = 'Please enter valid weight and height.';  
-    }  
+    resultElement.innerHTML = `Your BMI is ${bmi.toFixed(2)} - ${category}`;
+  } else {
+    resultElement.innerHTML = "Please enter valid weight and height.";
+  }
+}
+
+function resetForm() {  
+  document.getElementById('weight').reset();
+  document.getElementById('height').reset(); // Resets the form fields  
+  document.getElementById('result').innerText = ''; // Clears the result display  
 }  
-
-
